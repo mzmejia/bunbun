910b7ae0fdda2656f4493eb7638a1b9a5b4a725f
mzmejia <marlon@mzmejia.com>
2021-11-06 02:01:11 -0400

    Created a new structure for the item creator
    Played with the sturcture of the item creation so that the user stays on the same page at all times. I still cannot figure out the local storage which is stopping me from spanning the process across multiple pages like I intended. I have had to pivot my design to actually get make it usable, even though it is not what I was intending. It seems to me like this is more of a prototype then an interactive site, so I would like to figure out how to properly implement the different techniques needed to make it interactive rather than just a flushed out prototype.

--- main.js
+++ main.js
@@ -33,6 +33,39 @@ Primary Pseudocode for adding selections to an item to be stored in the cart
 */
 
 
+/* ---------------------------------------------TEST with local storage---------------------------------------------
+
+const storageInput = document.querySelector('.storage')
+const text = document.querySelector('.text')
+const button = document.querySelector('.button')
+const storedInput = localStorage.getItem('textInput')
+
+if (storageInput) {
+  text.textContent = storedInput
+}
+
+storage.input.addEventListener (input,letter => {
+  text.textContent = letter.target.value
+})
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('textInput', text.textContent)
+}
+
+button.addEventListener('click', saveToLocalStorage)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 //---------------------------------------------Flavor selection functions---------------------------------------------
 /* 
@@ -44,12 +77,25 @@ have a good idea on how to add a new element, so am planning on working on that 
 */
 
 
+//------------------------------------------------------------ Flavor selections
+
 //------------ Variables for original element
 function flavOriginal () {
   window.newFlavor = document.getElementById('flavorOriginal').innerHTML;
   console.log("Selected flavor is: " + newFlavor);
   window.newPic = document.getElementById('flavorOriginalPic').getAttribute("src");
   console.log(newPic);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
+  
 }
 
 //------------ Variables for blackberry element
@@ -58,6 +104,16 @@ function flavBlackberry () {
   console.log("Selected flavor is: " + newFlavor);
   window.newPic = document.getElementById('flavorBlackberryPic').getAttribute("src");
   console.log(newPic);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
 }
 
 //------------ Variables for walnut element
@@ -65,6 +121,16 @@ function flavWalnut () {
   window.newFlavor = document.getElementById('flavorWalnut').innerHTML;
   console.log("Selected flavor is: " + newFlavor);
   window.newPic = document.getElementById('flavorWalnutPic').getAttribute("src");
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
 }
 
 //------------ Variables for pumpkin spice element
@@ -73,7 +139,244 @@ function flavPumpkin () {
   console.log("Selected flavor is: " + newFlavor);
   window.newPic = document.getElementById('flavorPumpkinPic').getAttribute("src");
   console.log(newPic);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
 }
+
+//------------ Variables for caramel pecan element
+function flavCaramel () {
+  window.newFlavor = document.getElementById('flavorCaramel').innerHTML;
+  console.log("Selected flavor is: " + newFlavor);
+  window.newPic = document.getElementById('flavorCaramelPic').getAttribute("src");
+  console.log(newPic);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
+}
+
+//------------ Variables for gluten free element
+function flavGlutenFree () {
+  window.newFlavor = document.getElementById('flavorGlutenFree').innerHTML;
+  console.log("Selected flavor is: " + newFlavor);
+  window.newPic = document.getElementById('flavorGlutenFreePic').getAttribute("src");
+  console.log(newPic);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor); //change flavor
+  cartImage.src = (newPic);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
+}
+
+
+//------------------------------------------------------------ Glaze selections
+
+
+//------------ Variables for none glaze
+function glazeNone () {
+  window.newGlaze = document.getElementById('glazeNone').innerHTML;
+  console.log("Selected glaze is: " + newGlaze);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartGlaze')[0].innerHTML = ("Glaze: " + newGlaze); //change glaze
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+//------------ Variables for sugar glaze
+function glazeSugar () {
+  window.newGlaze = document.getElementById('glazeSugar').innerHTML;
+  console.log("Selected glaze is: " + newGlaze);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartGlaze')[0].innerHTML = ("Glaze: " + newGlaze); //change glaze
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+//------------ Variables for vanilla glaze
+function glazeVanilla () {
+  window.newGlaze = document.getElementById('glazeVanilla').innerHTML;
+  console.log("Selected glaze is: " + newGlaze);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartGlaze')[0].innerHTML = ("Glaze: " + newGlaze); //change glaze
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+//------------ Variables for choco glaze
+function glazeChoco () {
+  window.newGlaze = document.getElementById('glazeChoco').innerHTML;
+  console.log("Selected glaze is: " + newGlaze);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartGlaze')[0].innerHTML = ("Glaze: " + newGlaze); //change glaze
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+
+//------------------------------------------------------------ Quantity selections
+
+
+//------------ Variables for 1
+function quantity1 () {
+  window.newQuantity = document.getElementById('quantity1').innerHTML;
+  console.log("Selected glaze is: " + newQuantity);
+  window.newOrderTotal = document.getElementById('orderTotal').innerHTML;
+  window.newOrderTotalNumber = newQuantity * 1;
+  window.multiplier = 3;
+  window.theOrderTotal = newOrderTotalNumber * multiplier;
+  console.log("Order total is: " + theOrderTotal);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor + ' x' + newQuantity); //change flavor and quantity
+  document.getElementsByClassName('cartOrderTotal')[0].innerHTML = ('Order total: $' + theOrderTotal);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+//------------ Variables for 3
+function quantity3 () {
+  window.newQuantity = document.getElementById('quantity3').innerHTML;
+  console.log("Selected glaze is: " + newQuantity);
+  window.newOrderTotal = document.getElementById('orderTotal').innerHTML;
+  window.newOrderTotalNumber = newQuantity * 1;
+  window.multiplier = 3;
+  window.theOrderTotal = newOrderTotalNumber * multiplier;
+  console.log("Order total is: " + theOrderTotal);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor + ' x' + newQuantity); //change flavor and quantity
+  document.getElementsByClassName('cartOrderTotal')[0].innerHTML = ('Order total: $' + theOrderTotal);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+
+//------------ Variables for 6
+function quantity6 () {
+  window.newQuantity = document.getElementById('quantity6').innerHTML;
+  console.log("Selected glaze is: " + newQuantity);
+  window.newOrderTotal = document.getElementById('orderTotal').innerHTML;
+  window.newOrderTotalNumber = newQuantity * 1;
+  window.multiplier = 3;
+  window.theOrderTotal = newOrderTotalNumber * multiplier;
+  console.log("Order total is: " + theOrderTotal);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor + ' x' + newQuantity); //change flavor and quantity
+  document.getElementsByClassName('cartOrderTotal')[0].innerHTML = ('Order total: $' + theOrderTotal);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+}
+
+//------------ Variables for 12
+function quantity12 () {
+  window.newQuantity = document.getElementById('quantity12').innerHTML;
+  console.log("Selected glaze is: " + newQuantity);
+  window.newOrderTotal = document.getElementById('orderTotal').innerHTML;
+  window.newOrderTotalNumber = newQuantity * 1;
+  window.multiplier = 3;
+  window.theOrderTotal = newOrderTotalNumber * multiplier;
+  console.log("Order total is: " + theOrderTotal);
+  //Change interior of the cart element
+  document.getElementsByClassName('cartDescription')[0].innerHTML = (newFlavor + ' x' + newQuantity); //change flavor and quantity
+  document.getElementsByClassName('cartOrderTotal')[0].innerHTML = ('Order total: $' + theOrderTotal);
+  //Switch over/toggle to the glaze div
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'block';
+  var showItem = document.getElementById('itemNumber1');
+  var hideText = document.getElementById('placeholderText');
+  showItem.style.display = 'flex';
+  hideText.style.display = 'none';
+
+}
+
+
+
+
+
+//------------ Show and hide divs - used in the selected elements and the "go back" buttons
+
+function showFlavorDiv() {
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'block';
+  glazeDiv.style.display = 'none';
+  quantityDiv.style.display = 'none';
+}
+
+function showGlazeDiv() {
+  var flavorDiv = document.getElementById('mainFlavorDiv');
+  var glazeDiv = document.getElementById('mainGlazeDiv');
+  var quantityDiv = document.getElementById('mainQuantityDiv')
+  flavorDiv.style.display = 'none';
+  glazeDiv.style.display = 'block';
+  quantityDiv.style.display = 'none';
+}
+
+
+
+
 
 //------------ Change the interior html text to the correct variables (newflavor)
 //------------ Test for the flavor portion
@@ -96,7 +399,47 @@ function addCartDescription() {
 
 //---------------------------------------------Failed attempts and some information as to why---------------------------------------------
 
-  
+ /*----Hide and show divs with with code found on stackverflow. I didnt like how it worked, so decided to resturcture it
+and use different functions built into each selected Element. Also wanted to give users a chance to go back a Step and used a button to show 
+that action*/
+
+/*
+
+function hideMainDiv() {
+  var x = document.getElementById("mainFlavorDiv");
+  var y = document.getElementById("mainGlazeDiv");
+  var z = document.getElementById("mainQuantityDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+  if (x.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+*/
+
+
+//----Hide and show divs with jquery. Dont understand how to load it, so didnt work. lets use css instead
+/*
+$("#mainFlavorDiv").on('click', function() {
+  $('#mainFlavorDiv').fadeIn();
+  $('#mainGlazeDiv').fadeOut();
+});
+*/
+
+
+
+
+
+
+//------------------------------ Old ------------------------------
+
   
 //------------ Attempted to use local storage to create and save variables from other pages
 //------------ and then call them in the cart page, but it failed.
